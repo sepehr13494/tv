f2e712be20c1d730d0a55358c20947e560b85ff5
Sepehr <marash.sepehr@gmail.com>
2021-03-30 11:35:33 +0430

    flutter version.dart added

---
+++ lib/version.dart
@@ -0,0 +1,5 @@
+/*Flutter 2.0.1 • channel beta • https://github.com/flutter/flutter.git
+Framework • revision c5a4b4029c (4 weeks ago) • 2021-03-04 09:47:48 -0800
+Engine • revision 40441def69
+Tools • Dart 2.12.0
+*/
